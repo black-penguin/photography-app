c8772f3670993cf8b7598bd503fb7073426d0b2b
Austin Wheelwright <pyrotechnician25@gmail.com>
2017-06-26 11:13:19 -0600

    centered contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,14 +8,16 @@ export default class Contact extends Component
   render()
   {
     return (
-      <div className="form text-left">
-        <form autoComplete="on">
-          <input type="text" placeholder="Name" name="uname" required /><br />
-          <input type="text" id="number" name="number" placeholder="(385)225-5273" /><br />
-          <input type="text" id="email" name="email" placeholder="Email@domain.com" /><br />
-          <textarea id="conent" name="content" placeholder="Message"></textarea>
-          <input type="submit" value="Submit" />
-        </form>
+      <div className="contact">
+        <div className="form text-left">
+          <form autoComplete="on">
+            <input type="text" placeholder="Name" name="uname" required /><br />
+            <input type="text" id="number" name="number" placeholder="(385)225-5273" /><br />
+            <input type="text" id="email" name="email" placeholder="Email@domain.com" /><br />
+            <textarea id="conent" name="content" placeholder="Message"></textarea>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
